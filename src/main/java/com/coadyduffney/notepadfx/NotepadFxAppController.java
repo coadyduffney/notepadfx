7d1f0841b2adc2eec7dab0bd36c8c4f98de9b532
Coady Duffney <coadyduffney@gmail.com>
2022-04-04 14:25:01 -0300

    Implemented word wrap feature

--- src/main/java/com/coadyduffney/notepadfx/NotepadFxAppController.java
+++ src/main/java/com/coadyduffney/notepadfx/NotepadFxAppController.java
@@ -1,6 +1,7 @@
 package com.coadyduffney.notepadfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
@@ -9,13 +10,19 @@ public class NotepadFxAppController {
     private TextArea textArea;
 
     @FXML
+    private CheckMenuItem wordWrapMenuItem;
+
+    @FXML
     public void initialize() {
         AnchorPane.setLeftAnchor(textArea, 0.);
         AnchorPane.setBottomAnchor(textArea, 0.);
         AnchorPane.setRightAnchor(textArea, 0.);
         AnchorPane.setTopAnchor(textArea, 0.);
 
-//        textArea.setStyle("-fx-focus-color: -fx-control-inner-background ; -fx-faint-focus-color: -fx-control-inner-background ;");
+    }
 
+    @FXML
+    public void setWrapText() {
+        textArea.setWrapText(wordWrapMenuItem.isSelected());
     }
 }
